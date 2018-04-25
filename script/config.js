4680744063a2319dde1fbc083ccc595e30d10497
赵晗旭 <1948287545@>
2018-04-25 15:20:50 +0800

    test

---
+++ script/config.js
@@ -0,0 +1,7 @@
+const cfg = {
+  baseUrl: "http://47.104.103.114:8804/",//测试环境
+  // baseUrl: "http://47.104.103.114:8803/",//生产环境
+  pageSize: 20,
+  userCacheKEY: "userCache20171220",
+  dictionaryCacheKEY: "dictionaryCache20180103"
+}
