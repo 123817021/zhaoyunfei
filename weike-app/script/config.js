f25353a97f5cbdae4e273e398be1ae498a1b4f83
赵晗旭 <1948287545@>
2018-04-25 14:34:13 +0800

    地址

--- weike-app/script/config.js
+++ weike-app/script/config.js
@@ -1,5 +1,5 @@
 const cfg = {
-  baseUrl: "http://59.110.171.199:8803/",//测试环境
+  baseUrl: "http://47.104.103.114:8804/",//测试环境
   // baseUrl: "http://47.104.103.114:8803/",//生产环境
   pageSize: 20,
   userCacheKEY: "userCache20171220",
